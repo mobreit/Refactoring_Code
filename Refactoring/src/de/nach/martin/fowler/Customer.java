1dac211b8dd46c80be45555e16e4e755cf3ea50a
mobreit <marv.obr@gmail.com>
2014-05-21 13:53:31 +0200

    Replace Temp with Query für total Ammount wurde in die While-Schleife eingefügt

--- Refactoring/src/de/nach/martin/fowler/Customer.java
+++ Refactoring/src/de/nach/martin/fowler/Customer.java
@@ -16,7 +16,6 @@ public class Customer {
         return name;
     };
     public String statement() {
-        double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration enum_rentals = rentals.elements();	    
         String result = "Rental Record for " + this.getName() + "\n";
@@ -26,12 +25,22 @@ public class Customer {
             Rental each = (Rental) enum_rentals.nextElement(); 
             frequentRenterPoints += each.getFrequentRenterPoints();
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" + String.valueOf(each.getCharge()) + "\n";
-            totalAmount += each.getCharge();
+            result += "\t" + each.getMovie().getTitle()+ "\t" + 
+            String.valueOf(each.getCharge()) + "\n";
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
+        
+    }
+    private double getTotalCharge() {
+    	double result = 0.0;
+    	Enumeration enum_rentals = rentals.elements();
+    	while(enum_rentals.hasMoreElements()) {
+    		Rental each = (Rental) enum_rentals.nextElement();
+    		result += each.getCharge();
+    	}
+    	return result;
     }
 }
